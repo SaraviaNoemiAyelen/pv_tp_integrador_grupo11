051a8eb06a61982231d57595eba1b4c2391b8590
Isabelafk <mendoza9848248@gmail.com>
2025-07-03 16:35:16 -0300

    Se corrigio el css y el jsx de NavBar agregando nuevas funciones

--- tp_integrador_grupo11/src/components/NavBar.jsx
+++ tp_integrador_grupo11/src/components/NavBar.jsx
@@ -1,21 +1,62 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logoutUser } from "../redux/slices/userSlice.js";
 
 import "./NavBar.css";
 
 const NavBar = () => {
-    return (
-        <nav className="nav-bar">
-            <div className="links">
-                <ul>
-                    <li><Link to={"/"}>Inicio</Link></li>
-                    <li><Link to={"/favorites"}>Favoritos</Link></li>
-                </ul>
-            </div>
-            <div className="logo-nav">
-                <img src="/logo.png" alt="Logo" />
-            </div>
-        </nav>
-    );
+  const { currentUser, isAuthenticated } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    navigate("/login");
+  };
+
+  return (
+    <nav className="nav-bar">
+      <div className="nav-left">
+        <div className="logo-nav">
+          <img src="/logo.png" alt="Logo" />
+        </div>
+        {isAuthenticated && currentUser && (
+          <div className="user-info">
+            Bienvenido, {currentUser.name}
+          </div>
+        )}
+      </div>
+
+      <div className="nav-right">
+        <div className="links">
+          <ul>
+            <li>
+              <Link to="/" className="nav-button">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/favorites" className="nav-button">Favoritos</Link>
+            </li>
+            {!isAuthenticated && (
+              <>
+                <li>
+                  <Link to="/register" className="nav-button">Registrarse</Link>
+                </li>
+                <li>
+                  <Link to="/login" className="nav-button">Iniciar Sesión</Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+
+        {isAuthenticated && currentUser && (
+          <button onClick={handleLogout} className="logout-button">
+            Cerrar Sesión
+          </button>
+        )}
+      </div>
+</nav>
+  );
 };
 
 export default NavBar;
