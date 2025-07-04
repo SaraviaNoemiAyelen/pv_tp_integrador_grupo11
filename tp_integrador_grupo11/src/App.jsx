badaca5a4e26ce6753585f8f3585d67ac9a00dc9
Matias-Mamni5315 <matiasmamani998@gmail.com>
2025-07-04 13:14:55 -0300

    se modifico el app.jsx para agregar todas las nuevas funciones previamentes implementadas

--- tp_integrador_grupo11/src/App.jsx
+++ tp_integrador_grupo11/src/App.jsx
@@ -2,30 +2,89 @@ import { useState } from "react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar.jsx";
-import Home from "./components/Home.jsx";
+import HomePage from "./components/Home.jsx";
 import FavoritesPage from "./pages/FavoritesPage.jsx";
-
 import ProductDetailPage from "./pages/ProductDetailPage.jsx";
-import { Provider } from "react-redux";
-import Header from "./views/Header/Header.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 import ProductForm from "./services/ProductForm.jsx";
+import SearchResultsPage from "./pages/SearchResultsPage.jsx";
+
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import Header from "./views/Header/Header.jsx";
 import Footer from "./views/Footer/Footer.jsx";
 
 function App() {
   return (
     <>
-      <NavBar></NavBar>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="/productos/:id" element={<ProductDetailPage />} />
-        <Route path="/create-product" element={<ProductForm />} />
-        <Route path="/edit-product/:productId" element={<ProductForm />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <HomePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <>
+                <Header />
+                <HomePage />
+              </>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/favorites"
+          element={
+            <PrivateRoute>
+              <FavoritesPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/productos/:id"
+          element={
+            <PrivateRoute>
+              <ProductDetailPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/create-product"
+          element={
+            <PrivateRoute>
+              <ProductForm />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/edit-product/:productId"
+          element={
+            <PrivateRoute>
+              <ProductForm />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <PrivateRoute>
+              <SearchResultsPage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
-      <Footer></Footer>
+      <Footer />
     </>
   );
 }
